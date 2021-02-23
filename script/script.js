923e4acff66e72cb2060cc68085b78ad625f5120
Pipos <pipos@gmail.com>
2021-02-23 17:07:23 -0300

    at work 1

--- script/script.js
+++ script/script.js
@@ -1,8 +1,32 @@
 let Modal = {
-	open() {
-		document.querySelector(".modal-overlay").classList.add("active");
-	},
-	close() {
-		document.querySelector(".modal-overlay").classList.remove("active");
-	},
+    openClose() {
+        document.querySelector(".modal-overlay").classList.toggle("active");
+    },
+};
+
+const transactions = [
+    {
+        id: 1,
+        description: "Website",
+        amount: 500000,
+        date: "23/01/2021",
+    },
+    {
+        id: 2,
+        description: "Luz",
+        amount: -50000,
+        date: "23/01/2021",
+    },
+    {
+        id: 3,
+        description: "Internet",
+        amount: -20000,
+        date: "23/01/2021",
+    },
+];
+
+const Transaction = {
+    incomes() {},
+    expenses() {},
+    total() {},
 };
