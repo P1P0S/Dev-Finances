4bedb8fb48963bfcbba0edeb4dd9f13b26364039
Pipos <pipos@gmail.com>
2021-03-02 11:34:37 -0300

    at work

--- script/script.js
+++ script/script.js
@@ -1,59 +1,59 @@
 let Modal = {
-	openClose() {
-		document.querySelector(".modal-overlay").classList.toggle("active");
-	},
+    openClose() {
+        document.querySelector(".modal-overlay").classList.toggle("active");
+    },
 };
 
 const transactions = [
-	{
-		id: 1,
-		description: "Website",
-		amount: 500000,
-		date: "23/01/2021",
-	},
-	{
-		id: 2,
-		description: "Luz",
-		amount: -50000,
-		date: "23/01/2021",
-	},
-	{
-		id: 3,
-		description: "Internet",
-		amount: -20000,
-		date: "23/01/2021",
-	},
-	{
-		id: 4,
-		description: "App",
-		amount: 500000,
-		date: "30/01/2021",
-	},
+    {
+        id: 1,
+        description: "Website",
+        amount: 500000,
+        date: "23/01/2021",
+    },
+    {
+        id: 2,
+        description: "Luz",
+        amount: -50000,
+        date: "23/01/2021",
+    },
+    {
+        id: 3,
+        description: "Internet",
+        amount: -20000,
+        date: "23/01/2021",
+    },
+    {
+        id: 4,
+        description: "App",
+        amount: 500000,
+        date: "30/01/2021",
+    },
 ];
 
 const Transaction = {
-	incomes() {},
-	expenses() {},
-	total() {},
+    incomes() {},
+    expenses() {},
+    total() {},
 };
 
 const DOM = {
-	transactionsContainer: document.querySelector("#data-table tbody"),
+    transactionsContainer: document.querySelector("#data-table tbody"),
 
-	addTransaction(transaction, index) {
-		const tr = document.createElement("tr");
-		tr.innerHTML = DOM.innerHTMLTransaction(transaction);
-		DOM.transactionsContainer.appendChild(tr);
-	},
+    addTransaction(transaction, index) {
+        const tr = document.createElement("tr");
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+        DOM.transactionsContainer.appendChild(tr);
+    },
 
-	innerHTMLTransaction(transaction) {
-		const CSSclass = transaction.amount > 0 ? "income" : "expense";
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
-		const amount = Utils.formatCurrency(transaction.amount);
+        const amount = Utils.formatCurrency(transaction.amount);
 
-		const html = `
+        const html = `
         <td class="description">${transaction.description}</td>
-        <td class="${CSSclass}">${transaction.amount}</td>
+        <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
             <img
@@ -62,16 +62,25 @@ const DOM = {
             />
         </td>
         `;
-		return html;
+        return html;
 	},
+	
+	updateBalance()
 };
 
 const Utils = {
-	formatCurrency(value) {
-		const signal = Number(value) < 0 ? "-" : "";
-	},
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : "";
+        value = String(value).replace(/\D/g, "");
+        value = Number(value) / 100;
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+        });
+        return signal + value;
+    },
 };
 
 transactions.forEach((transaction) => {
-	DOM.addTransaction(transaction);
+    DOM.addTransaction(transaction);
 });
